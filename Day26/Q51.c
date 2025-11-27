6e24762fc93e1c87b0cb6cdf061516acb5b69d46
GauravNair007 <gnair4227@gmail.com>
2025-11-27 20:18:29 +0530

    DAY26

---
+++ Day26/Q51.c
@@ -0,0 +1,27 @@
+/*Q51 Write a program to print the following pattern:
+    5
+   45
+  345
+ 2345
+12345
+*/
+
+#include <stdio.h>
+
+int main() {
+    int i, j, spaces;
+
+    for (i = 5; i >= 1; i--) {
+
+        for (spaces = 1; spaces <= i - 1; spaces++) {
+            printf(" ");
+        }
+
+        for (j = i; j <= 5; j++) {
+            printf("%d", j);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
