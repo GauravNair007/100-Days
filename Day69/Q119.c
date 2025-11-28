7ba8af05a589cc48eef98a7c3085d0af102bd98d
GauravNair007 <gnair4227@gmail.com>
2025-11-28 21:30:48 +0530

    69

---
+++ Day69/Q119.c
@@ -0,0 +1,30 @@
+/*Q119 Write a program to take an integer array as input. Only one element will be repeated. 
+Print the repeated element. Try to find the result in one single iteration.*/
+
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Enter size of array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter array elements (only one repeated element, all others unique): ");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int repeated = 0;
+
+    for (int i = 0; i < n; i++) {
+        repeated ^= arr[i];
+    }
+
+    for (int i = 1; i <= n - 1; i++) {
+        repeated ^= i;
+    }
+
+    printf("Repeated element = %d\n", repeated);
+
+    return 0;
+}
