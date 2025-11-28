3e781885c0479491223c67c76227f3ebc29fdc9e
GauravNair007 <gnair4227@gmail.com>
2025-11-28 15:24:00 +0530

    DAY44

---
+++ Day44/Q88.c
@@ -0,0 +1,22 @@
+//Q88 Replace spaces with hyphens in a string.
+
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int i = 0;
+
+    printf("Enter a string: ");
+    scanf("%[^\n]s", str);
+
+    while(str[i] != '\0') {
+        if(str[i] == ' ') {
+            str[i] = '-';
+        }
+        i++;
+    }
+
+    printf("String after replacing spaces with hyphens: %s\n", str);
+
+    return 0;
+}
