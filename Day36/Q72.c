f78e036475eab178beec37f214aea9dcd7d10443
GauravNair007 <gnair4227@gmail.com>
2025-11-28 15:22:26 +0530

    DAY36

---
+++ Day36/Q72.c
@@ -0,0 +1,27 @@
+//Q72 Find the sum of all elements in a matrix.
+
+#include <stdio.h>
+
+int main() {
+    int rows, cols, i, j;
+    int matrix[10][10];
+    int sum = 0;
+
+    printf("Enter number of rows: ");
+    scanf("%d", &rows);
+
+    printf("Enter number of columns: ");
+    scanf("%d", &cols);
+
+    printf("Enter the elements of the matrix:\n");
+    for(i = 0; i < rows; i++) {
+        for(j = 0; j < cols; j++) {
+            scanf("%d", &matrix[i][j]);
+            sum += matrix[i][j];
+        }
+    }
+
+    printf("\nSum of all elements = %d\n", sum);
+
+    return 0;
+}
