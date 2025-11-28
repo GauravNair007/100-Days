9e7c882ff4abf035d486e10d7c8164db88a3bb2f
GauravNair007 <gnair4227@gmail.com>
2025-11-28 21:30:57 +0530

    70

---
+++ Day70/Q120.c
@@ -0,0 +1,29 @@
+/*Q120 Write a program to take a string input. Change it to sentence case.*/
+
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+int main() {
+    char str[1000];
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    str[strcspn(str, "\n")] = '\0';
+
+    int len = strlen(str);
+
+    if (len > 0) {
+
+        str[0] = toupper(str[0]);
+
+        for (int i = 1; i < len; i++) {
+            str[i] = tolower(str[i]);
+        }
+    }
+
+    printf("Sentence case: %s\n", str);
+
+    return 0;
+}
